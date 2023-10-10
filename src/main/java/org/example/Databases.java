e33b04b36a35def6a87ff9e8c8d045fb1f02e507
bareisha.darya <dariaboreysha@gmail.com>
2023-10-10 11:59:24 +0300

    REmoving yaml initialization

--- src/main/java/org/example/Databases.java
+++ src/main/java/org/example/Databases.java
@@ -1,19 +1,12 @@
 package org.example;
 
 
-import org.yaml.snakeyaml.Yaml;
-
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Map;
 
 public class Databases {
-    InputStream yamlDoc = getClass().getClassLoader().getResourceAsStream("application.yaml");
-    Yaml yaml = new Yaml();
-    Map data = yaml.load(yamlDoc);
 
     private static final String HOST = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USERNAME = "postgres";
--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -2,28 +2,25 @@ import com.github.javafaker.Faker;
 import org.example.Main;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.annotation.Testable;
-import org.yaml.snakeyaml.Yaml;
-
-import java.io.InputStream;
 import java.sql.SQLException;
-import java.util.Map;
 import java.util.Random;
 
 public class DatabaseTest {
 
     public DatabaseTest() throws SQLException {
     }
+
     Main main = new Main();
     Faker faker = new Faker();
     Random random = new Random();
 
     @Test
-    public void getData(){
-       main.getFullTable();
+    public void getData() {
+        main.getFullTable();
     }
+
     @Test
-    public void addNewUser(){
+    public void addNewUser() {
         String name = faker.name().firstName();
         String email = faker.internet().emailAddress();
         String pass = faker.internet().password();
@@ -31,26 +28,28 @@ public class DatabaseTest {
         String nameRes = main.getLastUserName();
         String emailRes = main.getLastUserEmail();
         String passRes = main.getLastUserPass();
-        Assertions.assertEquals(name,nameRes, "ER: " + name + ", AR: " + nameRes);
-        Assertions.assertEquals(email,emailRes, "ER: " + email +  ", AR: " + emailRes);
-        Assertions.assertEquals(passRes,pass, "ER: " + pass  +", AR: " + passRes);
+        Assertions.assertEquals(name, nameRes, "ER: " + name + ", AR: " + nameRes);
+        Assertions.assertEquals(email, emailRes, "ER: " + email + ", AR: " + emailRes);
+        Assertions.assertEquals(passRes, pass, "ER: " + pass + ", AR: " + passRes);
     }
+
     @Test
-    public void deleteLastUser(){
-       int before = main.getNumberOfRows();
-       main.deleteLastRow();
-       int after = main.getNumberOfRows();
-       Assertions.assertEquals(before-1,after,"The deletion is not correct");
+    public void deleteLastUser() {
+        int before = main.getNumberOfRows();
+        main.deleteLastRow();
+        int after = main.getNumberOfRows();
+        Assertions.assertEquals(before - 1, after, "The deletion is not correct");
     }
+
     @Test
-    public void changeUsersPassword(){
+    public void changeUsersPassword() {
         int numberOfRows = main.getNumberOfRows();
-        int id = random.nextInt(numberOfRows-1+1)+1;
+        int id = random.nextInt(numberOfRows - 1 + 1) + 1;
         String before = main.selectUserPassword(id);
         String newPass = faker.internet().password();
-        main.updatePassword(newPass,id);
+        main.updatePassword(newPass, id);
         String after = main.selectUserPassword(id);
-        Assertions.assertTrue((newPass.equals(after))&&(newPass!=before), "Password isn't changed correctly");
+        Assertions.assertTrue((newPass.equals(after)) && (newPass != before), "Password isn't changed correctly");
     }
 
 }
